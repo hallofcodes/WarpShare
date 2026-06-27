7adb79cc3f9ea5c15a9f492867798bcecb1ff3ad
dconco <onipededavid208@gmail.com>
2026-06-27 20:47:50 +0100

    updated

--- stores/user_current_shared_path.py
+++ stores/user_current_shared_path.py
@@ -1,7 +1,7 @@
-shared_path: str = ""
+path: str = ""
 
-def save_user_current_shared_path(path: str):
-   shared_path = path
+# def save_user_current_shared_path(path: str):
+#   shared_path = path
 
-def retrieve_user_current_shared_path() -> str:
-   return shared_path
+# def retrieve_user_current_shared_path() -> str:
+#   return shared_path
